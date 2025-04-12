8ee4ba7388afadab01d5f1470a65c9841a3b1783
SaSuKe123346 <sudeshxettry09@gmail.com>
2025-04-12 17:11:32 +1000

    biweekly

---
+++ Tutorial4Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4Ex2
+{
+    using System;
+
+    class Person
+    {
+        public string Details; // Single field to store name and age
+
+        public Person(string name, int age)
+        {
+            Details = $"Name: {name}, Age: {age}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new Person("Alex", 20);
+            Console.WriteLine(person1.Details);
+        }
+    }
+
+}
